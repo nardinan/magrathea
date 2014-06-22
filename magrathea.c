164e95e2c9a1e96e2432c32d1d99adf69023ba92
Andrea Nardinocchi <andrea@nardinan.it>
2014-06-23 00:31:34 +0200

    Some test functions have been implemented in the main function (just for test)

--- magrathea.c
+++ magrathea.c
@@ -16,6 +16,31 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "console.h"
+#include "rs232.h"
 int main (int argc, char *argv[]) {
+	struct s_console *console;
+	struct s_console_input input;
+	struct s_console_command commands[] = {
+		{
+			e_console_level_guest,
+			(struct s_console_parameter[]){
+				{"-t", "temperature", d_false, d_false, d_true},
+				{"-A", "apprendistato", d_false, d_false, d_true},
+				{"", "", d_false, d_false, d_false}
+			},
+			"prova", "comando prova, qui ci metti una descrizione", NULL, d_true
+		},
+		{e_console_level_guest, NULL, "ancora", "comando ancora, qui ci metti una descrizione", NULL, d_true},
+		{e_console_level_guest, NULL, "ancona", "comando ancona, qui ci metti una descrizione", NULL, d_true},
+		{e_console_level_guest, NULL, "", "", NULL, d_false}
+	};
+	f_memory_init();
+	f_console_init(&console, commands, STDIN_FILENO);
+	strcpy(console->prefix, "\rCMD>");
+	console->level = e_console_level_guest;
+	while (d_true) {
+		f_console_read(console, &input, STDOUT_FILENO, 0, 10);
+	}
+	f_memory_destroy();
 	return 0;
 }
