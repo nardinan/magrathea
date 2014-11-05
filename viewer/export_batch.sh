d17562d3418cf5d1f13fbde9fb9f692777e6e423
Andrea Nardinocchi <andrea@nardinan.it>
2014-11-05 15:34:18 +0100

    Some new parameters have been added

--- viewer/export_batch.sh
+++ viewer/export_batch.sh
@@ -4,11 +4,11 @@ do
 	cd ~/Projects/magrathea/viewer
 	for trb in 1 2; # add here all TRBs that you need
 	do
-		./magrathea_view.bin $parameter $trb 0 512 -x -k &
+		./magrathea_view.bin $parameter $trb 0 512 -x -k -m bad_channels.csv &
 	done
 	# this piece of code has been used during EQM tests in Shanghai (9-18 Sept 2014)
-	#cd ~/Projects/magrathea/compare
+	# cd ~/Projects/magrathea/compare
 	#./magrathea_compare.bin ~/Desktop/calibrations_TRB1 "$parameter"_TRB01cals 2,3,6,7,10,11,14,15,18,19,22,23 # 21 has been removed because it is bad
 	#./magrathea_compare.bin ~/Desktop/calibrations_TRB2 "$parameter"_TRB02cals 0,1,4,5,8,9,12,13,16,17,20,21,22
-	#cd ~/Projects/magrathea/
+	# cd ~/Projects/magrathea/
 done
