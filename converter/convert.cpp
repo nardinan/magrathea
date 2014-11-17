5b5958d106c48532d47038034ee86940a10d8496
andrea nardinocchi <andrea@nardinan.it>
2014-11-17 16:22:36 +0100

    Now all packages have been added to the TTree and a new flag has been setted if the CRC is wrong

--- converter/convert.cpp
+++ converter/convert.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "convert.h"
-int tree_adc[d_convert_ladders][d_package_channels], tree_trigger[d_convert_ladders];
+int tree_adc[d_convert_ladders][d_package_channels], tree_trigger[d_convert_ladders], tree_wrong_crc[d_convert_ladders];
 unsigned char v_convert_mode = d_package_dmg_workmode;
 struct s_convert_environment *f_convert_init(struct s_convert_environment *supplied, const char *prefix, int trb) {
 	char directory[PATH_MAX], postfix[d_string_buffer_size], path[PATH_MAX], h_name[d_string_buffer_size], t_name[d_string_buffer_size];
@@ -37,9 +37,8 @@ struct s_convert_environment *f_convert_init(struct s_convert_environment *suppl
 					snprintf(t_name, d_string_buffer_size, "adc%d/I", channel);
 					result->structure[ladder]->Branch(h_name, &(tree_adc[ladder][channel]), t_name);
 				}
-				snprintf(h_name, d_string_buffer_size, "t_Trigger");
-				snprintf(t_name, d_string_buffer_size, "trigger_id/I");
-				result->structure[ladder]->Branch(h_name, &(tree_trigger[ladder]), t_name);
+				result->structure[ladder]->Branch("t_Trigger", &(tree_trigger[ladder]), "trigger_id/I");
+				result->structure[ladder]->Branch("t_WrongCRC", &(tree_wrong_crc[ladder]), "wrong_crc/I");
 			} else
 				d_die(d_error_malloc);
 		}
@@ -62,6 +61,7 @@ int p_convert_insert_raw(struct s_convert_environment *environment, struct s_pac
 			for (channel = 0; channel < d_package_channels; ++channel)
 				tree_adc[selected_ladder][channel] = package->data.values.raw.values[ladder][channel];
 			tree_trigger[selected_ladder] = package->data.trigger_counter;
+			tree_wrong_crc[selected_ladder] = package->wrong_sumcheck;
 			environment->structure[selected_ladder]->Fill();
 		}
 	return result;
@@ -73,6 +73,7 @@ int p_convert_insert_nrm(struct s_convert_environment *environment, struct s_pac
 		for (channel = 0; channel < d_package_channels; ++channel)
 			tree_adc[ladder][channel] = package->data.values.nrm.ladders_data[ladder].values[channel];
 		tree_trigger[ladder] = package->data.trigger_counter;
+		tree_wrong_crc[ladder] = package->wrong_sumcheck;
 		environment->structure[ladder]->Fill();
 	}
 	return result;
@@ -104,7 +105,7 @@ int f_convert_read(const char *prefix, FILE *stream, int trb) {
 			if (((backup = f_package_analyze(&package, buffer, bytes))) && (backup > buffer)) {
 				bytes -= (backup-buffer);
 				memmove(buffer, backup, bytes);
-				if ((package.complete) && (package.trb == v_package_trbs[trb].code) && (!package.wrong_sumcheck)) {
+				if ((package.complete) && (package.trb == v_package_trbs[trb].code)) {
 					current_mode = package.data.kind;
 					if ((current_mode == d_package_nrm_workmode) || (current_mode == d_package_raw_workmode)) {
 						switch (current_mode) {
